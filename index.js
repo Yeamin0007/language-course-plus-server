ee5cda53bd5b90f4ce203549b4452ea6529fa365
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-10-25 01:50:29 +0600

    commit courses list

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT ||5000;
+app.use(cors());
+
+const courses = require('./data/courses.json')
+const details = require('./data/details.json')
+
+app.get('/',(req, res)=>{
+    res.send('Courses api running');
+});
+
+app.get('/courses', (req, res)=>{
+    res.send(courses)
+})
+
+
+
+app.listen(port, ()=>{
+    console.log('course plus server running on port', port);
+})
