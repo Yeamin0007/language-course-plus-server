c72ad1a8dc928a479f8591353546f347e5b2ec3d
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-10-25 22:10:31 +0600

    remove node modules

--- index.js
+++ index.js
@@ -4,19 +4,31 @@ const cors = require('cors');
 const port = process.env.PORT ||5000;
 app.use(cors());
 
+const categories = require('./data/categories.json')
 const courses = require('./data/courses.json')
-const details = require('./data/details.json')
 
 app.get('/',(req, res)=>{
     res.send('Courses api running');
 });
 
+app.get('/categories', (req, res)=>{
+    res.send(categories)
+})
+
 app.get('/courses', (req, res)=>{
     res.send(courses)
 })
 
-app.get('/details', (req, res)=>{
-    res.send(details)
+app.get('/category/:id', (req, res)=>{
+    const id = req.params.id;
+    const categoryCourse = courses.filter(c => c.id == id);
+    res.send(categoryCourse);
+})
+
+app.get('/courses/:id',(req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(c => c.id == id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, ()=>{
